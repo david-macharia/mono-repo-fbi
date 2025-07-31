a2554f5227b1bf3ca77971e71ed517986de7c2fc
david macharia <davidkiarie5861@gmail.com>
2025-07-31 03:52:59 +0300

    temporal host name

--- fbi-wanted-api/src/modules/base.module.ts
+++ fbi-wanted-api/src/modules/base.module.ts
@@ -23,7 +23,7 @@ import { PrismaService } from 'src/services/prisma.service';
       isGlobal: true,
       store: redisStore({
         socket: {
-          host: process.env.REDIS_HOST || '127.0.0.1', // 🔧 Hardcoded Redis host
+          host: 'redis' || '127.0.0.1', // 🔧 Hardcoded Redis host
           port: (process.env.REDIS_PORT || 6379) as number,
         },
       }),
